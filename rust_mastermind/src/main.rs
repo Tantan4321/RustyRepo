5aa76790471dc9b8ca5b52eedbae0065c9a09f87
Eastan Giebler <kupa528@yahoo.com>
2019-12-08 12:47:57 -0600

    Simple number guessing game

--- rust_mastermind/src/main.rs
+++ rust_mastermind/src/main.rs
@@ -1,12 +1,24 @@
 use std::io;
+use rand::Rng;
+use std::process::exit;
 
 fn main() {
     println!("Guess my number! It's between 1 and 10 inclusive!");
+    let rand_number: u32 = rand::thread_rng().gen_range(1,11);
 
-    let mut buffer = String::new();
+    let mut guess = String::new();
     let stdin = io::stdin();
 
-    stdin.read_line(&mut buffer).expect("Input failed");
+    stdin.read_line(&mut guess).expect("Input failed");
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
-    println!("Your guess: {}", buffer);
+    println!("Your guess: {}", guess);
+    if guess == rand_number {
+        println!("Correct! You win!");
+    }else {
+        println!("Incorrect! The number was {}", rand_number);
+    }
+
+    exit(0);
 }
