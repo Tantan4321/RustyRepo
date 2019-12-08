a8e3d3d4ee900b4817d438e860dc158d1cfcc736
Eastan Giebler <kupa528@yahoo.com>
2019-12-08 12:22:46 -0600

    Add simple user input and respond

--- rust_mastermind/src/main.rs
+++ rust_mastermind/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess my number! It's between 1 and 10 inclusive!");
+
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+
+    stdin.read_line(&mut buffer).expect("Input failed");
+
+    println!("Your guess: {}", buffer);
 }
