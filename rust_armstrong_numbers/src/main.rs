1fc9247cdda0621825031e1f1b9471e25380a969
Eastan Giebler <kupa528@yahoo.com>
2019-12-08 23:36:08 -0600

    Implement vectorizer method from mastermind project
    - also added gitignore

--- rust_armstrong_numbers/src/main.rs
+++ rust_armstrong_numbers/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn vectorize(n: String) -> Vec<u32>{
+    let mut ret: Vec<u32> = Vec::new();
+    for i in 0..n.len(){
+        let splice = &n[i..i+1];
+        ret.push(splice.to_string().parse().unwrap());
+    }
+    ret
+}
