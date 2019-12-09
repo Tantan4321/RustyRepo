4d9a84356278cf0308a74672725de36de5ff0aa4
Eastan Giebler <kupa528@yahoo.com>
2019-12-08 23:40:56 -0600

    Working Armstrong number script :))

--- rust_armstrong_numbers/src/main.rs
+++ rust_armstrong_numbers/src/main.rs
@@ -1,5 +1,37 @@
+use std::io;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please type in a number and I'll tell you if it's an Armstrong number:");
+    let stdin = io::stdin();
+    let mut input = String::new();
+    stdin.read_line(&mut input).expect("Input failed");
+    let input = input.trim();
+
+    //check if input is actually a number
+    let number: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("That's not a number!");
+            exit(0);
+        }
+    };
+
+    //convert number into separated list
+    let nums: Vec<u32> = vectorize(input.to_string());
+
+    let power: u32 = input.len() as u32;
+    let mut sum: u32 = 0;
+    for digit in nums {
+        sum += digit.pow(power);
+    }
+
+    if number == sum {
+        println!("{} is an Armstrong number.", number);
+    } else {
+        println!("{} is not an Armstrong number.", number);
+    }
+    exit(0);
 }
 
 fn vectorize(n: String) -> Vec<u32>{
