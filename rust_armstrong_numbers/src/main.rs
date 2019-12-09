9b3f3c3f75b690b6ddb8b624c6ef7db4976c9c98
Eastan Giebler <kupa528@yahoo.com>
2019-12-08 23:41:03 -0600

    Formatting

--- rust_armstrong_numbers/src/main.rs
+++ rust_armstrong_numbers/src/main.rs
@@ -34,10 +34,10 @@ fn main() {
     exit(0);
 }
 
-fn vectorize(n: String) -> Vec<u32>{
+fn vectorize(n: String) -> Vec<u32> {
     let mut ret: Vec<u32> = Vec::new();
-    for i in 0..n.len(){
-        let splice = &n[i..i+1];
+    for i in 0..n.len() {
+        let splice = &n[i..i + 1];
         ret.push(splice.to_string().parse().unwrap());
     }
     ret
